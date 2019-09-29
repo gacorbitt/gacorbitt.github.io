058bff9b30564af27467e6a79074579242c5ba4b
gacorbitt <gacorbitt02@gmail.com>
2019-09-29 13:48:51 -0500

    js update

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 var year = new Date().getFullYear();
 document.getElementById('year').textContent = year;
+
+var lastUpdate = document.lastModified;
+document.getElementById('lastUpdate').textContent = lastUpdate;
