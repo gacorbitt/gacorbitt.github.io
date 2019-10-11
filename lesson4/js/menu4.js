618292e492916fa8476d8ed857c4a51d9e837c09
gacorbitt <gacorbitt02@gmail.com>
2019-10-10 23:52:35 -0500

    preston public

--- lesson4/js/menu4.js
+++ lesson4/js/menu4.js
@@ -1,23 +1,4 @@
 function toggleMenu() {
-    console.log(document.getElementById("hamburger").classList);
-    document.getElementById("hamburger").classList.toggle("hide");
+    console.log(document.getElementById("primaryNav").classList);
+    document.getElementById("primaryNav").classList.toggle("hide");
 }
-
-
-
-
-
-/*function loadDate(){
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
-    var dateObject = new Date();
-    var day = days[dateObject.getDay()];
-    var dateNumber =dateObject.getDate();
-    var month = monthNames[dateObject.getMonth()];  
-    var year = dateObject.getFullYear();
-    
-    document.getElementById("year").innerHTML = year; 
-    document.getElementById("lastModified").innerHTML = day+", "+dateNumber+" "+month+" "+year;
-    }
