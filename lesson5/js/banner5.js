f321c96b716371fc6d7020ee303be966d8225d87
gacorbitt <gacorbitt02@gmail.com>
2019-10-20 14:44:32 -0500

    day

--- lesson5/js/banner5.js
+++ lesson5/js/banner5.js
@@ -1,7 +1,7 @@
 function banner() {
 	var d = new Date();
 	var n = d.getDay();
-	if (n == 6) {
+	if (n == 5) {
 		document.getElementById('banner').style.display = 'block';
 	} else {
 		document.getElementById('banner').style.display = 'none';
