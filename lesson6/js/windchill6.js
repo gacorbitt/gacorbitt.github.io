2d614cfbdea2238092878e680f3fbd93ac72f31d
gacorbitt <gacorbitt02@gmail.com>
2019-10-29 14:48:51 -0500

    chill

--- lesson6/js/windchill6.js
+++ lesson6/js/windchill6.js
@@ -1,12 +1,11 @@
-function windChill() {
+
     var ht = document.getElementById("highTemp").textContent;
     var ws = document.getElementById("windSpeed").textContent;
-    var wc = 35.74 + (0.6215 * ht) - (35.75 * Math.pow(ws, 0.16)) + (0.4275 * (ht * Math.pow(ws, 0.16)));
+    var wc = (35.74 + (0.6215*ht)- (35.75*(Math.pow(ws, 0.16))) + (0.4275*ht*Math.pow(ws, 0.16)));
 
-    if (ht <= 50 && ws > 3) {
+    if (ht < 51 && ws > 3) {
         document.getElementById("chilly").innerHTML = wc.toFixed(1) + " &deg;F";
     }
     else {
-        document.getElementById("chilly").innerHTML = "N/A";
+        document.getElementById("chilly").textContent = "N/A";
     }
-}
