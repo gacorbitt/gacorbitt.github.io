63a3e720d31073181d7db763c322fec0a19f7ef8
gacorbitt <gacorbitt02@gmail.com>
2019-11-16 14:25:55 -0600

    lessonnine

---
+++ lesson9/js/windchill9.js
@@ -0,0 +1,10 @@
+var ht = document.getElementById("highTemp").textContent;
+    var ws = document.getElementById("windSpeed").textContent;
+    var wc = (35.74 + (0.6215*ht)- (35.75*(Math.pow(ws, 0.16))) + (0.4275*ht*Math.pow(ws, 0.16)));
+
+    if (ht < 51 && ws > 3) {
+        document.getElementById("chilly").innerHTML = wc.toFixed(1) + " &deg;F";
+    }
+    else {
+        document.getElementById("chilly").textContent = "N/A";
+    }
